d8287703312f1ac0493c9f6066b6eccbe4f8c6e9
Eroax <eroaxe.business@gmail.com>
2022-11-04 03:48:41 -0600

    Setup Auth_Popup scene for Encapsulated Re/Authentication
    Needed a simple way to force authentication and check the authentication so I added a popup for it that pauses the tree to avoid any issues with calling authentication and enable authenticating at the same time.

---
+++ Scripts/Auth_Popup.gd
@@ -0,0 +1,50 @@
+extends WindowDialog
+
+onready var http : HTTPRequest = $http
+
+
+signal token_valid()
+
+
+func _ready():
+	
+	http.connect("request_completed", self, "http_check_complete")
+	
+
+# Connected to Get_Auths got_token function.  Assumes it has a valid token 
+func got_valid_token():
+	
+	get_tree().paused = false
+	hide()
+	
+
+func check_token():
+	
+	if Globals.creds.token == "This":
+		
+		push_error("Nonexistent Token")
+		
+		return
+		
+	
+	http.request('https://id.twitch.tv/oauth2/validate', 
+				["Authorization: Bearer " + Globals.creds.token])
+	
+
+# Connected to HTTPRequests request_completed signal for checking if the token that was passed to twitch is valid 
+func http_check_complete(result: int, response_code: int, headers: PoolStringArray, body: PoolByteArray):
+	
+	var info = parse_json(body.get_string_from_utf8())
+	
+#	Checks that the result was correct and that the correct info was supplied to ensure that the token is valid and avoid twitch weirdness
+	if result == 200 and info.has("scopes") and info.has("login"):
+		
+		emit_signal("token_valid")
+		
+		return true
+		
+	
+	get_tree().paused = true
+	
+	popup()
+	
