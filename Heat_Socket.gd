b70059f892d88ff71b2505b630b6759a9c4ff8c9
Eroax <eroaxe.business@gmail.com>
2022-11-09 18:48:55 -0700

    Fixed receiving Data on Heat_Socket

--- Heat_Socket.gd
+++ Heat_Socket.gd
@@ -8,22 +8,25 @@ var heat_url := "wss://heat-api.j38.net/channel/"
 signal heat_data(info)
 
 
+func connect_to_heat(channel_id):
 	
 	connect("data_received", self, "socket_received_data")
 	
 	connect("connection_established", self, "socket_connected")
+	
+	print(heat_url + str(channel_id))
+	
+	connect_to_url(heat_url + str(channel_id))
 	
 
 func socket_connected(proto):
 	
 	print("Heat Socket Connected")
 	
-	is_connected = true
-	
 
 func socket_received_data():
 	
-	var info = parse_json(get_peer(0).get_packet().get_string_from_utf8())
+	var info = get_peer(1).get_packet().get_string_from_utf8()
 	
 	emit_signal("heat_data", info)
 	
