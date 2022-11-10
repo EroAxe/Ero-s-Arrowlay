a67f9259278fb4a672e6a81b191eb40925f07843
Eroax <eroaxe.business@gmail.com>
2022-11-09 18:34:59 -0700

    Removed Testing Code from Heat_Socket

--- Heat_Socket.gd
+++ Heat_Socket.gd
@@ -5,20 +5,9 @@ class_name Heat_Socket
 # Used to hold the URL for heat that we pass the Channel ID to listen through
 var heat_url := "wss://heat-api.j38.net/channel/"
 
-var is_connected := false
-
 signal heat_data(info)
 
-func _ready():
-	
-	verify_ssl = false
-	
 
-func connect_to_heat():
-	
-#	connect_to_url(heat_url + str(Globals.settings.channel_id))
-	
-	connect_to_url(heat_url + str(144606537))
 	
 	connect("data_received", self, "socket_received_data")
 	
